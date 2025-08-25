8b850922d518a86e5bc1c274f5ccf174f3ef9920
abdorrahmani <mohammad02abdorrahmani@gmail.com>
2025-08-25 15:33:06 +0330

    ✨ feat: Enhance chat server with TCP listener and connection handling
    - Implemented TCP listener on port 8080 for incoming connections
    - Added connection acceptance and handling logic in the main function
    - Integrated server's HandleConnection function for concurrent client management

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"chat-server/internal/server"
+	"fmt"
+	"log"
+	"net"
+)
+
 func main() {
-	// TODO:: implement chat server
+	s := server.NewChatServer()
+
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Printf("Error listening on port 8080: %s\n", err)
+		return
+	}
+
+	defer listener.Close()
+
+	fmt.Println("Listening on port 8080")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Error accepting connection: %s\n", err)
+			continue
+		}
+
+		go server.HandleConnection(conn, s)
+	}
 }
