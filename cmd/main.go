52d5466dba8ceb940a1fb11655782f2929eaf42f
abdorrahmani <mohammad02abdorrahmani@gmail.com>
2025-08-28 18:05:13 +0330

    ✨ feat: add configuration management for chat server
    - Introduced config.yml for chat server settings including port, password, and message limits
    - Implemented LoadConfig function to read and parse configuration using Viper
    - Updated main.go to utilize configuration settings for TCP listener

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"chat-server/internal/config"
 	"chat-server/internal/server"
 	"fmt"
 	"log"
@@ -8,9 +9,15 @@ import (
 )
 
 func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		fmt.Printf("error loading config: %v\n", err)
+		return
+	}
+
 	s := server.NewChatServer()
 
-	listener, err := net.Listen("tcp", ":8080")
+	listener, err := net.Listen("tcp", ":"+cfg.PORT)
 	if err != nil {
 		fmt.Printf("Error listening on port 8080: %s\n", err)
 		return
---
+++ internal/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	PORT             string `yaml:"PORT"`
+	RequirePassword  bool   `yaml:"RequirePassword"`
+	Password         string `yaml:"Password"`
+	MaxMessageLength int    `yaml:"MaxMessageLength"`
+	MaxClients       int    `yaml:"MaxClients"`
+	RateLimit        int    `yaml:"RateLimit"`
+	EnableLogging    bool   `yaml:"EnableLogging"`
+	LogFile          string `yaml:"LogFile"`
+}
+
+func LoadConfig() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, fmt.Errorf("fatal error config file: %w", err)
+	}
+
+	var config Config
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return nil, fmt.Errorf("fatal error config file: %w", err)
+	}
+	return &config, nil
+}
